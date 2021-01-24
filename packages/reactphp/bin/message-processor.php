947c5b552e82e25cf38abe0adfb9537ad0de1a33
stableford <stableford@stablefords-MacBook-Pro.local>
2021-01-24 23:21:39 +1100

    chore: move around

---
+++ packages/reactphp/bin/message-processor.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once dirname(__DIR__).'/vendor/autoload.php';
+
+use React\EventLoop\Factory;
+use React\Socket\Server as SocketServer;
+use Pseudo\PhpServiceTemplate\AppServer;
+
+$loop = Factory::create();
+
+$appServer = new AppServer(loop: $loop);
+
+$socketServer = new SocketServer($appServer->getUri(), $loop);
+
+$httpServer = $appServer->build();
+$httpServer->listen($socketServer);
+
+echo 'Listening on ' . str_replace('tcp:', 'http:', $socketServer->getAddress()) . "\n";
+
+$loop->run();
